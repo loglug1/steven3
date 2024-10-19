4071a022b6a9d7bfa3045d1eba333dfc1a114b02
Josh Erwin <joshua.erwin13@gmail.com>
2024-10-19 14:05:29 -0500

    added a lock to the boss room

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    [Header("Inscribed")]
+    public bool platformToGround = false;
+    [Tooltip("If true, once the player is above it, the platform transforms into a solid ground object.")]
+    public GameObject groundToReplace;
+
     [Header("Dynamic")]
     public PlayerController player;
     public float distToTop;
     public Collider coll;
+    public Vector3 lowerPoint;
 
     void Awake() {
+                
         if ((player = Main.GET_PLAYER()) && (player != null)) {
             coll = GetComponent<Collider>();
             distToTop = coll.bounds.extents.y;
@@ -20,6 +27,12 @@ public class Platform : MonoBehaviour
     void FixedUpdate() {
         if ((Input.GetAxis("Vertical") != -1) && (player.transform.position.y > transform.position.y + distToTop)) { //if player is above platform and not holding down
             coll.enabled = true;
+            if(platformToGround==true){
+                lowerPoint= transform.position;
+                lowerPoint.y-=0.25f;
+                Instantiate(groundToReplace,lowerPoint,transform.rotation);
+                Destroy(this);
+            }
         } else {
             coll.enabled = false;
         }
