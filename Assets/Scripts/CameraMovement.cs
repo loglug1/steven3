cc546568ef5e142691a228552a749ada979c6237
Josh Erwin <joshua.erwin13@gmail.com>
2024-10-19 13:16:13 -0500

    added camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,22 +5,48 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [Header("Inscribed")]
-    public float CameraIncrementX = 39.5f;
-    public float CameraIncrementY = 22f;
+    public float cameraIncrementX = 39.5f;
+    public float cameraIncrementY = 22f;
+    public GameObject player;
     
     [Header("Dynamic")]
     public float camWidth;
     public float camHeight;
+    public Vector3 camPosition;//gameObject.transform.position
+    public float radiusX;
+    public float radiusY;
+    public Vector3 playerPos;
 
     void Awake()
     {
         camHeight=Camera.main.orthographicSize;
         camWidth=camHeight*Camera.main.aspect;
+        camPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerPos = player.transform.position;
+        if(playerPos.x > camPosition.x + camWidth){
+            camPosition.x += cameraIncrementX;
+            transform.position = camPosition;
+            Debug.Log("Player Left Screen Right");
+        }
+        if(playerPos.x < camPosition.x - camWidth){
+            camPosition.x -= cameraIncrementX;
+            transform.position = camPosition;
+            Debug.Log("Player Left Screen Left");
+        }
+        if(playerPos.y > camPosition.y + camHeight){
+            camPosition.y += cameraIncrementY;
+            transform.position = camPosition;
+            Debug.Log("Player Left Screen Up");
+        }
+        if(playerPos.y < camPosition.y - camHeight){
+            camPosition.y -= cameraIncrementY;
+            transform.position = camPosition;
+            Debug.Log("Player Left Screen Down");
+        }
     }
 }
