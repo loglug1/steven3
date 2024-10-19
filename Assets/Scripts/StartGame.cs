35f49c8a48438e5fb5bd92865ce4bb4234043c40
Josh Erwin <joshua.erwin13@gmail.com>
2024-10-19 18:00:22 -0500

    Asset Changes
    To Player Model and Start Screen

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+public Button startButton;
+
+	void Start () {
+		Button btn = startButton.GetComponent<Button>();
+		btn.onClick.AddListener(OnClick);
+	}
+
+	void OnClick(){
+        SceneManager.LoadScene("__Scene_0");//whatever the start scene is
+		Debug.Log ("start button clicked");
+	}
+}
