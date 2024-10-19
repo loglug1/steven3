0644827c87cbdac8cba7a84d264bb3ae120ad518
Camden McKay <cam.mckay9@gmail.com>
2024-10-19 15:40:57 -0500

    worked

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -6,26 +6,40 @@ public class HealthController : MonoBehaviour
     public float maxHealth = 50f;
     public HealthBarController healthBar;
     public float currHealth = 50f;
+    public Color mainColor;
+    public Color lowColor;
     
     // [Header("Dynamic")]
 
     void Awake() {
         healthBar.maxValue = maxHealth;
         healthBar.value = currHealth;
+
+        healthBar.color = mainColor;
     }
 
     public void Damage(float amount) {
         currHealth -= amount;
         healthBar.value = currHealth;
+        if (currHealth / maxHealth <= 0.2) {
+            healthBar.color = lowColor;
+        }
         if (currHealth <= 0) {
-            if (GetComponent<PlayerController>() != null) {
+            PlayerController p;
+            EnemyController e;
+            if ((p = GetComponent<PlayerController>()) != null) {
                 Main.GameOver();
+            } else if ((e = GetComponent<EnemyController>()) != null) {
+                e.Die();
             }
         }
     }
     public void Heal(float amount) {
         currHealth += amount;
         healthBar.value = currHealth;
+        if (currHealth / maxHealth > 0.2) {
+            healthBar.color = mainColor;
+        }
     }
 
     public void OnTriggerStay(Collider c) {
