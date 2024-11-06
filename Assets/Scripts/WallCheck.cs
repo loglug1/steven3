a8c02b8368cd5d48dae0479f3804f6e7d755ec00
Josh Erwin <147878375+hypwr@users.noreply.github.com>
2024-11-05 22:15:23 -0600

    Update WallCheck.cs

--- Assets/Scripts/WallCheck.cs
+++ Assets/Scripts/WallCheck.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class WallCheck : MonoBehaviour
 {
     public bool WALL;
+    public bool wallSpawned;
+
+    void Update(){
+        // if(wallSpawned == false && )
+    }
+
+    void OnCollisionEnter(Collision other){
+        // if(other == /*wall collider object*/){
+        //     //check if wall bool is equal, if not spawn wall
+        // }
+    }
+
 }
