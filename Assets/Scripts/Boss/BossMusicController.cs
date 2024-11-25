704e6cd7a594a539cd4e691ae232819ce344d787
Camden McKay <cam.mckay9@gmail.com>
2024-11-24 22:00:32 -0600

    fixed music and door behavior on boss spawner

--- Assets/Scripts/Boss/BossMusicController.cs
+++ Assets/Scripts/Boss/BossMusicController.cs
@@ -3,14 +3,10 @@ using UnityEngine;
 
 public class BossMusicController : MonoBehaviour
 {
-    bool playing = false;
     public AudioClip bossMusic;
-    void FixedUpdate() {
-        if (CameraMovement.IsOnScreen(transform.position) && !playing) {
-            playing = true;
-            AudioSource aS = Camera.main.GetComponent<AudioSource>();
-            aS.clip = bossMusic;
-            aS.Play();
-        }
+    void Awake() {
+        AudioSource aS = Camera.main.GetComponent<AudioSource>();
+        aS.clip = bossMusic;
+        aS.Play();
     }
 }
